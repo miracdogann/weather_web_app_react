4d1611606c2a886d583dad3cd13d2ad22aefa594
Miraç Doğan <miracdogan7247@gmail.com>
2024-10-17 14:32:55 +0300

    İlk commit

---
+++ src/Components/SearchInput.js
@@ -0,0 +1,49 @@
+import { useState , useContext } from "react"
+import weatherContext from "../Context/weatherContext";
+import  "./style/searchInput.css"
+
+
+const SearchInput = () => {
+    const [city,setCity] = useState("");
+    const {setCity:fetchWeatherData} = useContext(weatherContext); // burda setCiyt ye fetchWeatherData adını verdik
+
+    const inputChange = (e)=> {
+        // console.log(e.target.value)
+        setCity(e.target.value)
+    }
+    console.log(city)
+    const handleSearch = () => {
+        if(!city.trim()){
+            alert("Lütfen bir şehir girin!")
+            return;
+        }
+        fetchWeatherData(city);
+        setCity("");// input alanını temizle
+    }
+    
+    return(
+        <div className="searchContainer">
+            <h1 className="headerText">Hava Durumu Sorgulama</h1>
+            <div className="search-main">
+                <input
+                    name ="searchWeather"
+                    placeholder="Şehir Giriniz"
+                    onChange={inputChange}
+                    value={city}
+                />
+                <button
+                    onClick={handleSearch}
+                >
+                    Ara
+                </button>
+            </div>
+        </div>
+    )
+}
+
+
+export default SearchInput;
+
+
+
+
